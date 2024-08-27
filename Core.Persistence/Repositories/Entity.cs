a1e0513bec86a785cb33ce7504085bebd8a98685
emreinan <emreinannn@gmail.com>
2024-08-27 20:39:15 +0300

    refactor/core.persistence - add IEntityTimestamps

--- Core.Persistence/Repositories/Entity.cs
+++ Core.Persistence/Repositories/Entity.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Core.Persistence.Repositories;
 
-public class Entity<TId>
+public class Entity<TId> : IEntityTimestamps
 {
     public TId Id { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime? UpdatedDate { get; set; }
-    public DateTime? DeletedDate { get; set; }
+	public DateTime CreatedDate { get; set; }
+	public DateTime? UpdatedDate { get; set; }
+	public DateTime? DeletedDate { get; set; }
 
-    public Entity()
+	public Entity()
     {
         Id = default;
     }
