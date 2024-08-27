dbd31ba9e90a814668093f99a84e0d65b655e260
emreinan <emreinannn@gmail.com>
2024-08-27 19:55:25 +0300

    feat/core.persistence - paging and IRepository,IAsyncRepository added

---
+++ Core.Persistence/Paging/IQueryablePaginateExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging;
+
+public static class IQueryablePaginateExtensions
+{
+	public static async Task<Paginate<T>> ToPaginateAsync<T>(
+		this IQueryable<T> source,
+		int index,
+		int size,
+		CancellationToken cancellationToken = default)
+	{
+		var count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
+		var items = await source.Skip(index * size).Take(size).ToListAsync(cancellationToken).ConfigureAwait(false);
+
+		return new Paginate<T>
+		{
+			Index = index,
+			Size = size,
+			Count = count,
+			Items = items,
+			Pages = (int)Math.Ceiling(count / (double)size)
+		};
+	}
+	public static Paginate<T> ToPaginate<T>(this IQueryable<T> source, int index, int size)
+	{
+		int count = source.Count();
+		var items = source.Skip(index * size).Take(size).ToList();
+
+		Paginate<T> list =
+			new()
+			{
+				Index = index,
+				Size = size,
+				Count = count,
+				Items = items,
+				Pages = (int)Math.Ceiling(count / (double)size)
+			};
+		return list;
+	}
+}
---
+++ Core.Persistence/Paging/Paginate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Paging;
+
+public class Paginate<T>
+{
+	public Paginate()
+	{
+		Items = Array.Empty<T>();
+	}
+
+	public int Index { get; set; } // Page number
+	public int Size { get; set; } // Number of items per page
+	public int Count { get; set; } // Number of items in the current page
+	public int Pages { get; set; }  // Total number of pages
+	public IList<T> Items { get; set; }
+
+	public bool HasPrevious => Index > 0; // Check if there is a previous page
+	public bool HasNext => Index + 1 < Pages; // Check if there is a next page
+
+}
