f3874aa5ce49ffbfc3f3e7a3dbb5e52ea901fa37
emreinan <emreinannn@gmail.com>
2024-08-27 20:00:20 +0300

    feat/core.persistence - dynamic query implemented

---
+++ Core.Persistence/Dynamic/Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic;
+
+public class Filter
+{
+    public string Field { get; set; } 
+    public string? Value { get; set; } 
+    public string Operator { get; set; } // =, !=, >, <, >=, <=, LIKE, IN, NOT IN
+	public string? Logic { get; set; } // AND, OR
+
+	public IEnumerable<Filter>? Filters { get; set; }
+
+    public Filter()
+    {
+        Field = string.Empty;
+        Operator = string.Empty;
+    }
+
+    public Filter(string field, string @operator)
+    {
+        Field = field;
+        Operator = @operator;
+    }
+}
