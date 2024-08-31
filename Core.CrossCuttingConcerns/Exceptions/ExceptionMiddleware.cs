5e3c7ecf39c8d33a7d2a7f65bb2181ecdce0215d
emreinan <emreinannn@gmail.com>
2024-08-31 10:18:09 +0300

    feat/Core.CrossCuttingConserns Global Exception Handler implemented

---
+++ Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions;
+
+public class ExceptionMiddleware
+{
+	private readonly RequestDelegate _next;
+	private readonly HttpExceptionHandler _httpExceptionHnadler;
+
+	public ExceptionMiddleware(RequestDelegate next)
+	{
+		_next = next;
+		_httpExceptionHnadler = new HttpExceptionHandler();
+	}
+
+	public async Task InvokeAsync(HttpContext context)
+	{
+		try
+		{
+			await _next(context);
+		}
+		catch (Exception exception)
+		{
+			await HandleExceptionAsync(context.Response, exception);
+		}
+	}
+
+	private Task HandleExceptionAsync(HttpResponse response, Exception exception)
+	{
+		response.ContentType = "application/json";
+		_httpExceptionHnadler.Response = response;
+		return _httpExceptionHnadler.HandleExceptionAsync(exception);
+	}
+}
---
+++ Core.CrossCuttingConcerns/Exceptions/Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ProblemDetailsExtensions
+{
+	// This is a generic method that serializes the given ProblemDetails object to a JSON string.
+	public static string AsJson<TProblemDetail>(this TProblemDetail problemDetail)
+		where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(problemDetail); 
+
+}
