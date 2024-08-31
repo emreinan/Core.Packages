d19f5693422b53415044e24484665999836eef44
emreinan <emreinannn@gmail.com>
2024-08-31 11:36:13 +0300

    feat/Core.CrossCuttingConserns mediatr pipeline fluent validation implemented

---
+++ Core.Application/Pipelines/Validation/RequestValidationBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationException = Core.CrossCuttingConcerns.Exceptions.Types.ValidationException; // Kendi yazdığımız ValidationException.
+
+namespace Core.Application.Pipelines.Validation;
+
+public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+	where TRequest : IRequest<TResponse>
+{
+	private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+	public RequestValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+	{
+		_validators = validators;
+	}
+
+	public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+	{
+		var context = new ValidationContext<object>(request);
+		var errors = _validators
+			.Select(v => v.Validate(context))
+			.SelectMany(result => result.Errors)
+			.Where(failure => failure != null)
+			.GroupBy(
+			keySelector: x => x.PropertyName,
+			resultSelector: (propertyName, errors) =>
+			new ValidationExceptionModel { Errors = errors.Select(e => e.ErrorMessage), Property = propertyName }).ToList();
+
+		if (errors.Any())
+			throw new ValidationException(errors);
+
+		TResponse response = await next();
+		return response;
+	}
+}
+
---
+++ Core.CrossCuttingConcerns/Exceptions/HttpProblemDetails/ValidationProblemDetails.cs
@@ -0,0 +1,18 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+
+public class ValidationProblemDetails : ProblemDetails
+{
+    public IEnumerable<ValidationExceptionModel> Errors { get; init; }
+    public ValidationProblemDetails(IEnumerable<ValidationExceptionModel> errors)
+	{
+		Title = "Request Validation Error";
+		Detail = "One or more validation errors occurred.";
+		Status = StatusCodes.Status400BadRequest;
+		Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+		Errors = errors;
+	}
+}
